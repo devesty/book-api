9ae0bba413f0c3ef45529644ab7c48a1fb9fce0b
devesty <estherharriet2@gmail.com>
2023-07-31 21:15:17 +0100

    Second commit

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const http = require('http');
-// const hostname = 'localhost';
-// const port = 3000;
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World ');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at   http://${hostname}:${port}/`);
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -43,7 +30,7 @@ let books = [
 ]
 
 // This is a route to create a new book
-app.post('/api/books', (req, res) => {
+app.post('/api/postbooks', (req, res) => {
   const {name, author} = req.body;
   if (!name || !author) {
     return res.status(400).json({error: 'name and author are required'})
@@ -56,11 +43,33 @@ app.post('/api/books', (req, res) => {
 });
 
 
+// This is a route for getting all books
+app.get('/api/getbooks', (req, res) => {
+  debugger
+  console.log(req);
+  return res.json(books);
+});
 
 
+// This is a route for deleting a particular book
+//{id} is a parameter representing the unique ID of the book to be deleted.
+app.delete('/api/books/:id', (req, res) => {
 
+ const { id } = req.params;
 
+// This line uses the findIndex() method to search for the index of the book with the specific id in the books array
+// The parseInt(id) is used to convert the id from the request parameters to an integer
+  const bookIndex = books.findIndex((book) => book.id === parseInt(id));
+  if (bookIndex === -1) {
+    return res.status(404).json({error: 'Book not found'});
+  }
 
+  // This line uses the splice() method to remove one book from the books array.
+  // the [0] is used to extract the first and only element from the array which is the deleted book
+  const deletedBook = books.splice(bookIndex, 1)[0]
+
+return res.json(deletedBook)
+})
 
 
 app.listen(PORT, () => {
